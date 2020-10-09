94d796ba10ce5962ddfc89795fed39421cf055b8
Eric <eric22@inbox.ru>
2020-10-09 11:31:27 +0300

    some actions

---
+++ script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const block = document.createElement('div');
+block.classList.add('block');
+block.textContent = 'HELLO';
+document.querySelector('.block1').append(block);
+document.querySelector('.block2').append(block);
+document.body.append(block);    // сработает на последний элемент присоединение
+
+const boxes = document.querySelectorAll('.box');
+for (let item of boxes) {
+    item.style.background = 'green';
+}
